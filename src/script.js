b14bfe1dcc22d21c8990d243cf4e97b20ab11a74
Arthurmendescouto <153393682+Arthurmendescouto@users.noreply.github.com>
2024-03-11 07:43:09 -0300

    atualização

---
+++ src/script.js
@@ -0,0 +1,16 @@
+
+const product=document.getElementById('imgProduct');
+const imagem=document.querySelector("#imagem")
+
+function black(){
+    document.querySelector('img').src="../img/1.png"
+}
+function blue(){
+    document.querySelector('img').src="../img/2.png"
+}
+    function yellow(){
+        document.querySelector('img').src="../img/3.png"
+        }
+        function green(){
+            document.querySelector('img').src="../img/4.png"
+            }
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'back':'#F0F2F5',
+        'backphone':'#FEFEFE',
+        'phone1':'#555F6B',
+        'phone2':'#8EA9BE',
+        'phone3':'#FFC750',
+        'phone4':'#065116'
+
+      },
+      spacing: {
+        '500':'500px',
+        '300':'300px',
+        '550':'550px'
+      }
+    },
+  },
+  plugins: [],
+}
+
